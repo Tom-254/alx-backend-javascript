d6e582be2b446259ce885f9fa93e0693a6c07fd8
Tom-254 <211mseeratiego@gmail.com>
2023-05-16 16:04:48 +0000

    0x05. NodeJS Basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+console.log("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("readable", () => {
   const chunk = process.stdin.read();
 
   if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
+    console.log(`Your name is: ${chunk}`);
   }
 });
 
 process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+  console.log("This important software is now closing\n");
 });
